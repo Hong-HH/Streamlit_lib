d709a319cdbd7e87fa858becd9c094207fd528b2
Hong-HH <a1guskn7@gmail.com>
2021-12-25 21:38:00 +0900

    add

--- app.py
+++ app.py
@@ -4,18 +4,30 @@ import folium
 
 def main() :
     st.title('테스트 페이지 입니다.')
-    with st.echo():
-        # center on Liberty Bell
-        m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+    
 
-        # add marker for Liberty Bell
-        tooltip = "Liberty Bell"
-        folium.Marker(
-            [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-        ).add_to(m)
+    lib = ['folium', 'observable']
+    select_lib = st.sidebar.selectbox('라이브러리를 선택해주세요', lib )
 
-        # call to render Folium map in Streamlit
-        folium_static(m)
+    if select_lib == 'folium' :
+        st.subheader('folium')
+        choice_eg = st.sidebar.radio('선택해주세요', ['예시', '예시2'])
+        if choice_eg == '예시' :
+            with st.echo():
+                # center on Liberty Bell
+                m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+
+                # add marker for Liberty Bell
+                tooltip = "Liberty Bell"
+                folium.Marker(
+                    [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+                ).add_to(m)
+
+                # call to render Folium map in Streamlit
+                folium_static(m)
+    
+    elif select_lib == 'observable' :
+        pass
 
 
 if __name__ == '__main__' :
